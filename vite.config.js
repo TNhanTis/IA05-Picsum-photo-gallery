3d63559a1d7fb699469b04946bb926f358167155
TNhanTis <trongnhan18052005@gmail.com>
2025-11-17 16:14:48 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/IA05-Picsum-photo-gallery-app/",
+  base: "/IA05-Picsum-photo-gallery/",
 });
